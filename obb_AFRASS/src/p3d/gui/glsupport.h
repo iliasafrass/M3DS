f3a9a5a33bcc8cad721a078702692549199be61c
Afrass Ilias <ilias.afrass@etudiant.univ-lille1.fr>
2018-04-29 17:04:42 +0200

    TP9 M3DS

---
+++ obb_AFRASS/src/p3d/gui/glsupport.h
@@ -0,0 +1,24 @@
+#ifndef GLSUPPORT_H
+#define GLSUPPORT_H
+
+/*!
+*
+* @file
+*
+* @brief
+* @author F. Aubert
+*
+*/
+
+#include <cmath>
+
+#include <iostream>
+#ifdef WITH_GLEW
+#include <GL/glew.h>
+#endif
+#include <QGLWidget>
+
+
+
+#endif // GLSUPPORT_H
+
